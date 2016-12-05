ef0b075f56835acf571b4d5fbe901b3b19699bb5
Tanat T <map@MacBook-Pro-Destroyer.local>
2016-12-06 02:02:48 +0700

    Last Commit

--- app/mockData.js
+++ app/mockData.js
@@ -1,214 +1,76 @@
 const Artists = [
   {
-    name: "Breakbot",
-    background: "http://jgnatch-dropshare.s3.amazonaws.com/Screen-Shot-2016-03-01-15-29-06/Screen-Shot-2016-03-01-15-29-06.png",
+    name: "Sleeping",
+    background: "https://www.dropbox.com/s/u4hqmhtcwliao4c/ewfrjh0gvty-annie-spratt.jpg",
     songs: [
       {
-        title: "Star Tripper",
-        album: "Still Waters",
-        albumImage: "http://www.clashmusic.com/sites/default/files/field/image/Breakbot%20%20Still%20Waters.jpg",
+        title: "Sweet Dream",
+        album: "Dream Catcher",
+        albumImage: "https://www.dropbox.com/s/vvvtiuo2eiu5kl3/7os_26cb1wo-griffin-keller.jpg",
         url: "https://api.soundcloud.com/tracks/254196631/stream?client_id=8a754483a114344c70ab15f20a5035ab",
       },
       {
-        title: "You Should Know (feat. Ruckazoid)",
-        album: "Still Waters",
-        albumImage: "http://images.junostatic.com/full/CS2032801-02A-BIG.jpg",
+        title: "Waterfall",
+        album: "Still Water",
+        albumImage: "https://www.dropbox.com/s/bv75ca2zr7cgm0k/ewfdaprfjwe-stefan-kunze.jpg",
         url: "https://api.soundcloud.com/tracks/99534378/stream?client_id=8a754483a114344c70ab15f20a5035ab",
       },
       {
-        title: "Bedtime Stories Mix",
-        album: "Mixtape",
-        albumImage: "https://edbangerla.files.wordpress.com/2013/07/artworks-000052534531-4p2uyd-t500x500.jpg",
+        title: "Granite",
+        album: "Calm Mountain",
+        albumImage: "https://www.dropbox.com/s/2045cxhysfuqs1b/f8e2tks5n04-darius-soodmand.jpg?dl=0",
         url: "https://api.soundcloud.com/tracks/99516198/stream?client_id=8a754483a114344c70ab15f20a5035ab",
       },
       {
-        title: "Nightcall",
-        album: "By Your Side",
-        albumImage: "https://fanart.tv/fanart/music/eb6de5f6-98f8-4b5a-bfdc-f87fa4936baa/artistbackground/-4f70ac2a1f207.jpg",
+        title: "Grass",
+        album: "Sound Grass",
+        albumImage: "https://www.dropbox.com/s/6warsk02rf2lgmn/fhxp17axoek-arto-marttinen.jpg?dl=0",
         url: "https://api.soundcloud.com/tracks/9738949/stream?client_id=8a754483a114344c70ab15f20a5035ab",
-      },
-      {
-        title: "Baby",
-        album: "By Your Side",
-        albumImage: "https://upload.wikimedia.org/wikipedia/en/4/48/Pnau_alternate_cover.jpg",
-        url: "https://api.soundcloud.com/tracks/9737435/stream?client_id=8a754483a114344c70ab15f20a5035ab",
       },
     ]
   },
   {
-    name: "The Beatles",
-    background: "http://jgnatch-dropshare.s3.amazonaws.com/Screen-Shot-2016-03-01-15-20-40/Screen-Shot-2016-03-01-15-20-40.png",
+    name: "Working",
+    background: "https://www.dropbox.com/s/eturyyjn7qsthwy/6g0kjwnbhxg-luis-llerena.jpg",
     songs: [
       {
-        title: "Something",
-        album: "Abbey Road",
+        title: "Firetime",
+        album: "Rage Fight",
+        albumImage: "https://www.dropbox.com/s/uhig3a3xg8s39ln/jzkgpml_8xi-lukas-budimaier.jpg?dl=0",
         url: "https://mp3l.jamendo.com/?trackid=1314632&format=mp31&from=app-97dab294",
       },
       {
-        title: "Hey Bulldog",
-        album: "Yellow Submarine",
+        title: "Nearsight",
+        album: "Yellow Light",
+        albumImage: "https://www.dropbox.com/s/qxd0rbv634vktmv/ndqa4d5nl0k-stephy-pariande.jpg?dl=0",
         url: "https://mp3l.jamendo.com/?trackid=1312012&format=mp31&from=app-97dab294",
       },
       {
-        title: "Happiness is a Warm Gun",
-        album: "The Beatles",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "Maggie Mae",
-        album: "Let It Be",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "Polythene Pam",
-        album: "Abbey Road",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "Being For The Benefit Of Mr. Kite!",
-        album: "Sgt. Pepper's Lonely Hearts Club Band",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "I'm Only Sleeping",
-        album: "Revolver",
+        title: "Imagination",
+        album: "Fantasy Book",
+        albumImage: "https://www.dropbox.com/s/dca8nk0enyoxp3u/rf5sv5dv1zc-arto-marttinen.jpg?dl=0",
         url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
       },
     ]
   },
   {
-    name: "Daft Punk",
-    background: "http://jgnatch-dropshare.s3.amazonaws.com/Screen-Shot-2016-03-01-15-25-13/Screen-Shot-2016-03-01-15-25-13.png",
+    name: "Driving",
+    background: "https://www.dropbox.com/s/ssfpmgyh5c0azx0/q_wg5fe_f5a-ezra-jeffrey.jpg",
     songs: [
       {
-        title: "Giorgio",
+        title: "Comfy",
         album: "Random Access Memories",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
+        url: "https://www.dropbox.com/s/4f6f1s6h1oe76dq/rzzeljw8dwc-ales-krivec.jpg?dl=0",
       },
       {
-        title: "Face to Face",
-        album: "Daft Club",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
+        title: "Wait",
+        album: "Traffic Jam",
+        url: "https://www.dropbox.com/s/iy0mln4qkfmjr45/us_loktu5la-amy-hanley.jpg?dl=0",
       },
       {
-        title: "Robot Rock",
-        album: "Human After All",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "Ouverture",
-        album: "Daft Club",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "Alive",
-        album: "Musique Vol 1",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "Crescendolls",
-        album: "Discovery",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-    ]
-  },
-  {
-    name: "Pink Floyd",
-    background: "http://jgnatch-dropshare.s3.amazonaws.com/Screen-Shot-2016-03-01-15-23-47/Screen-Shot-2016-03-01-15-23-47.png",
-    songs: [
-      {
-        title: "Marooned",
-        album: "The Division Bell",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "Sorrow",
-        album: "Delicate Sound of Thunder",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "Yet Another Movie",
-        album: "A Momentary Lapse of Reason",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "One of the Few",
-        album: "The Final Cut",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-    ]
-  },
-  {
-    name: "Andy Allo",
-    background: "http://jgnatch-dropshare.s3.amazonaws.com/Screen-Shot-2016-03-01-15-31-35/Screen-Shot-2016-03-01-15-31-35.png",
-    songs: [
-      {
-        title: "Fly Away (feat. The Tones)",
-        album: "UnFresh",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-    ]
-  },
-  {
-    name: "Jamiroquai",
-    background: "http://jgnatch-dropshare.s3.amazonaws.com/Screen-Shot-2016-03-01-15-33-17/Screen-Shot-2016-03-01-15-33-17.png",
-    songs: [
-      {
-        title: "Seven Days in Sunny June",
-        album: "Dyanmite",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "Space Cowboy",
-        album: "The Return of the Space Cowboy",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-    ]
-  },
-  {
-    name: "Illya Kuryaki & The Valderramas",
-    background: "http://jgnatch-dropshare.s3.amazonaws.com/Screen-Shot-2016-03-01-15-35-26/Screen-Shot-2016-03-01-15-35-26.png",
-    songs: [
-      {
-        title: "Funky Futurista",
-        album: "Chances",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "Discovery Buda",
-        album: "Versus",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "Jaguar House",
-        album: "Chaco",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "Safari Espiritual",
-        album: "Chances",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "Expedicion al Klama Hama",
-        album: "Versus",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-    ]
-  },
-  {
-    name: "Soda Stereo",
-    background: "http://cnnchile.com/uploads/imagenes/14344020424867_breaking.jpg",
-    songs: [
-      {
-        title: "Un Misil En Mi Placard",
-        album: "Comfort y Musica Para Volar",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
-      },
-      {
-        title: "Cuando Pase El Temblor",
-        album: "Me Veras Volver",
-        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
+        title: "Hill",
+        album: "Slide Road",
+        url: "https://www.dropbox.com/s/n7ih1eoxgqsb43j/usfowyo1qew-kalen-emsley.jpg?dl=0",
       },
     ]
   },
