d7389edbc0cc6c68a5765c29c7e4eaecfce8a880
Tanat T <map@MacBook-Pro-Destroyer.local>
2016-12-06 02:48:09 +0700

    Last Commit

--- app/mockData.js
+++ app/mockData.js
@@ -1,76 +1,79 @@
 const Artists = [
   {
     name: "Sleeping",
-    background: "https://www.dropbox.com/s/u4hqmhtcwliao4c/ewfrjh0gvty-annie-spratt.jpg",
+    background: "https://www.mx7.com/i/93b/1SbcDc.jpeg",
     songs: [
       {
         title: "Sweet Dream",
         album: "Dream Catcher",
-        albumImage: "https://www.dropbox.com/s/vvvtiuo2eiu5kl3/7os_26cb1wo-griffin-keller.jpg",
+        albumImage: "https://www.mx7.com/i/d75/8OYzbY.jpeg",
         url: "https://api.soundcloud.com/tracks/254196631/stream?client_id=8a754483a114344c70ab15f20a5035ab",
       },
       {
         title: "Waterfall",
         album: "Still Water",
-        albumImage: "https://www.dropbox.com/s/bv75ca2zr7cgm0k/ewfdaprfjwe-stefan-kunze.jpg",
+        albumImage: "https://www.mx7.com/i/9ca/gEchJB.jpeg",
         url: "https://api.soundcloud.com/tracks/99534378/stream?client_id=8a754483a114344c70ab15f20a5035ab",
       },
       {
         title: "Granite",
         album: "Calm Mountain",
-        albumImage: "https://www.dropbox.com/s/2045cxhysfuqs1b/f8e2tks5n04-darius-soodmand.jpg?dl=0",
+        albumImage: "https://www.mx7.com/i/c14/xBUcLX.jpeg",
         url: "https://api.soundcloud.com/tracks/99516198/stream?client_id=8a754483a114344c70ab15f20a5035ab",
       },
       {
         title: "Grass",
         album: "Sound Grass",
-        albumImage: "https://www.dropbox.com/s/6warsk02rf2lgmn/fhxp17axoek-arto-marttinen.jpg?dl=0",
+        albumImage: "https://www.mx7.com/i/c90/UrYnDM.jpeg",
         url: "https://api.soundcloud.com/tracks/9738949/stream?client_id=8a754483a114344c70ab15f20a5035ab",
       },
     ]
   },
   {
     name: "Working",
-    background: "https://www.dropbox.com/s/eturyyjn7qsthwy/6g0kjwnbhxg-luis-llerena.jpg",
+    background: "https://www.mx7.com/i/d2b/rLMO0r.jpegg",
     songs: [
       {
         title: "Firetime",
         album: "Rage Fight",
-        albumImage: "https://www.dropbox.com/s/uhig3a3xg8s39ln/jzkgpml_8xi-lukas-budimaier.jpg?dl=0",
+        albumImage: "https://www.mx7.com/i/cf4/QNoBNo.jpeg",
         url: "https://mp3l.jamendo.com/?trackid=1314632&format=mp31&from=app-97dab294",
       },
       {
         title: "Nearsight",
         album: "Yellow Light",
-        albumImage: "https://www.dropbox.com/s/qxd0rbv634vktmv/ndqa4d5nl0k-stephy-pariande.jpg?dl=0",
+        albumImage: "https://www.mx7.com/i/d93/pSFqMA.jpeg",
         url: "https://mp3l.jamendo.com/?trackid=1312012&format=mp31&from=app-97dab294",
       },
       {
         title: "Imagination",
         album: "Fantasy Book",
-        albumImage: "https://www.dropbox.com/s/dca8nk0enyoxp3u/rf5sv5dv1zc-arto-marttinen.jpg?dl=0",
+        albumImage: "https://www.mx7.com/i/c6b/MpaeKg.jpeg",
         url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
       },
     ]
   },
   {
     name: "Driving",
-    background: "https://www.dropbox.com/s/ssfpmgyh5c0azx0/q_wg5fe_f5a-ezra-jeffrey.jpg",
+    background: "https://www.mx7.com/i/cb5/DnGExF.jpeg",
     songs: [
       {
         title: "Comfy",
         album: "Random Access Memories",
-        url: "https://www.dropbox.com/s/4f6f1s6h1oe76dq/rzzeljw8dwc-ales-krivec.jpg?dl=0",
+        albumImage: "https://www.mx7.com/i/b56/7e26oT.jpeg",
+        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
       },
       {
         title: "Wait",
         album: "Traffic Jam",
-        url: "https://www.dropbox.com/s/iy0mln4qkfmjr45/us_loktu5la-amy-hanley.jpg?dl=0",
+        albumImage: "https://www.mx7.com/i/17e/UeudDb.jpeg",
+        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
       },
       {
         title: "Hill",
         album: "Slide Road",
-        url: "https://www.dropbox.com/s/n7ih1eoxgqsb43j/usfowyo1qew-kalen-emsley.jpg?dl=0",
+        albumImage: "https://www.mx7.com/i/b22/iDqg0x.jpeg",
+        url: "https://www.freesound.org/data/previews/208/208096_3767678-lq.mp3",
       },
     ]
   },
