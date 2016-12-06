bf61181e6e2997f82ae922f868854f3db0dc837e
Tanat T <map@MacBook-Pro-Destroyer.local>
2016-12-06 14:31:16 +0700

    Last Commit

--- app/components/artists/ArtistList.js
+++ app/components/artists/ArtistList.js
@@ -26,7 +26,7 @@ class ArtistList extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Artists
+          Categories
         </Text>
         <ListView
           dataSource={this.state.dataSource}
